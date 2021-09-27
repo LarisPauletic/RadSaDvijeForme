892d839ebe1f9d24a34e39087356d4c962d99119
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-09-27 09:27:16 +0200

    Add files via upload

---
+++ RadSaVišeFormi/RadSaVišeFormi/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadSaVišeFormi
+{
+    class Student
+    {
+        private string ime, prezime, brojIndeksa, smjer;
+        private DateTime datumRodjenja;
+
+        private string Ime { get => ime; set => ime = value; }
+        private string Prezime { get => prezime; set => prezime = value; }
+        private string BrojIndeksa { get => brojIndeksa; set => brojIndeksa = value; }
+        private string Smjer { get => smjer; set => smjer = value; }
+        private DateTime DatumRodjenja { get => datumRodjenja; set => datumRodjenja = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+            
+        }
+    }
+}
---
+++ RadSaVišeFormi/RadSaVišeFormi/Studenti.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadSaVišeFormi
+{
+    public partial class Studenti : Form
+    {
+        public Studenti()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDodaj_Click(object sender, EventArgs e)
+        {
+            UnosNovogStudenta myForm = new UnosNovogStudenta();
+            this.Hide();
+            myForm.ShowDialog();
+            this.Close();
+        }
+    }
+}
